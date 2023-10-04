c1971993c61f48179fa05f93044853824aa4088e
wilsondsn <info@wilsondsn.cl>
2023-10-04 09:26:40 -0300

    Actualización script de botonera

--- js/custom.js
+++ js/custom.js
@@ -4,16 +4,25 @@ $(function() {
 $("#oame-button").click(function(){
   $("#apoyos").removeClass("show");
   $("#programas").removeClass("show");
+  $("#glosario").removeClass("show");
 });
 
 $("#apoyos-button").click(function(){
   $("#oame").removeClass("show");
   $("#programas").removeClass("show");
+  $("#glosario").removeClass("show");
 });
 
 $("#programas-button").click(function(){
   $("#apoyos").removeClass("show");
   $("#oame").removeClass("show");
+  $("#glosario").removeClass("show");
+});
+
+$("#glosario-button").click(function(){
+  $("#apoyos").removeClass("show");
+  $("#oame").removeClass("show");
+  $("#programas").removeClass("show");
 });
 
 
